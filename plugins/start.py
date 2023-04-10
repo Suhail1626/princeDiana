eba548524bd92a88e7aab2b10bb3560e0bfcd248
Suhail1626 <104824273+Suhail1626@users.noreply.github.com>
2023-04-10 14:39:35 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,7 +88,7 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 Owner", url = https://t.me/BetterProffesor),
+                    InlineKeyboardButton("😊 Owner", url="https://t.me/BetterProffesor),
                     InlineKeyboardButton("🔒 Close", callback_data = "close")
                 ]
             ]
